62a237695a29c7a7ed04df8bd60e56412f814992
c c sreenidhin <ccsreenidhin@gmail.com>
2017-01-16 16:26:00 +0530

    workshop

---
+++ Java-Training/IO/src/Login.java
@@ -0,0 +1,17 @@
+
+public class Login {
+	public void login()
+	{
+		System.out.println("enter name");
+		s=obj.next();
+		System.out.println("enter pswd");
+		s1=obj.next();
+		if(s.equels("qwer")
+		{
+			if(s1.equels("123"))
+				System.out.println("welcome");
+		}
+		else
+			System.out.println("wrong input");
+	}
+}
---
+++ Java-Training/IO/src/Test.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class Test {
+	
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Scanner obj=new Scanner(System.in);
+		String s,s1;
+		String q;
+		System.out.println("enter l to login");
+		q=obj.next();
+		if(q=='l')
+			Login();
+		
+	}
+	
+
+}
